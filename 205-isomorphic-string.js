65468f14cc33086ba5a2db2891297dec7252a48a
kiqueangulo <kiqueangulo@gmail.com>
2022-11-15 15:37:01 -0500

    State problem 205

---
+++ 205-isomorphic-string.js
@@ -0,0 +1,18 @@
+/**
+ * Given two strings s and t, determine if they are isomorphic.
+ * Two strings s and t are isomorphic if the characters in s can be replaced to get t.
+ *
+ * All occurrences of a character must be replaced with another character while preserving
+ * the order of characters. No two characters may map to the same character, but
+ * a character may map to itself.
+ *
+ * Constrains:
+ * t.length == s.length
+ */
+
+const isIsomorphic = function (s, t) {}
+
+const s = "egg",
+  t = "add"
+
+console.log(isIsomorphic(s, t)) // Expected: true

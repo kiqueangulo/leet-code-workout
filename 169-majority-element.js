c034d216961c27d53a31a47bdf6e32e675dac1b6
kiqueangulo <kiqueangulo@gmail.com>
2022-11-16 17:36:00 -0500

    State problem 169

---
+++ 169-majority-element.js
@@ -0,0 +1,14 @@
+/**
+ * Given an array nums of size n, return the majority element.
+ * The majority element is the element that appears more than ⌊n / 2⌋ times.
+ * You may assume that the majority element always exists in the array.
+ *
+ * Constrains:
+ *      n == nums.length
+ */
+
+const majorityElement = function (nums) {}
+
+const nums = [3, 2, 3]
+
+console.log(majorityElement(nums)) // Expected: 3

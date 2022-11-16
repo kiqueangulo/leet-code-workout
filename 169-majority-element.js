937052f285b3c8fd6402c165682a3c9e7a8e3971
kiqueangulo <kiqueangulo@gmail.com>
2022-11-16 17:55:21 -0500

    Problem 169 solved with 65 ms and 45.3 MB, excellent performance

--- 169-majority-element.js
+++ 169-majority-element.js
@@ -7,8 +7,22 @@
  *      n == nums.length
  */
 
-const majorityElement = function (nums) {}
+const majorityElement = function (nums) {
+  const set = {}
+  let moreRepeated = [0, 0]
 
-const nums = [3, 2, 3]
+  for (let num of nums) {
+    if (!set[num]) set[num] = 1
+    else set[num] += 1
+  }
 
-console.log(majorityElement(nums)) // Expected: 3
+  for (let pair of Object.entries(set)) {
+    if (pair[1] > moreRepeated[1]) moreRepeated = pair
+  }
+
+  if (moreRepeated[1] > nums.length / 2) return moreRepeated[0]
+}
+
+const nums = [2, 2, 1, 1, 1, 2, 2]
+
+console.log(majorityElement(nums)) // Expected: 2

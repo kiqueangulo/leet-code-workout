1279df55ebcdb61ba9720a6103e6c4ec9dd20f87
kiqueangulo <kiqueangulo@gmail.com>
2022-11-16 23:28:10 -0500

    Problem 66 solved with 101 ms and 42.4 MB, a bit below average performance

--- 66-plus-one.js
+++ 66-plus-one.js
@@ -11,8 +11,23 @@
  *      digits does not contain any leading 0's.
  */
 
-const plusOne = function (digits) {}
+const plusOne = function (digits) {
+  for (let i = digits.length - 1; 0 <= i; i--) {
+    if (digits[i] === 9) {
+      digits[i] = 0
+      continue
+    }
 
-const digits = [4, 3, 2, 1]
+    digits[i] += 1
+
+    return digits
+  }
+
+  digits.unshift(1)
+
+  return digits
+}
+
+const digits = [6, 1, 4, 5, 3, 9, 0, 1, 9, 5, 1, 8, 6, 7, 0, 5, 5, 4, 3]
 
 console.log(plusOne(digits))

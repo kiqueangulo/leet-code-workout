43d6e0817233048e5867bcd0be9a782f8c5fdbdf
kiqueangulo <kiqueangulo@gmail.com>
2022-11-17 02:51:36 -0500

    State problem 217

---
+++ 217-contains-duplicate.js
@@ -0,0 +1,10 @@
+/**
+ * Given an integer array nums, return true if any value appears at least twice in the array,
+ * and return false if every element is distinct.
+ */
+
+const containsDuplicate = function (nums) {}
+
+const nums = [1, 2, 3, 1]
+
+console.log(containsDuplicate(nums)) // Expected: true

1b1e92bb11f8c942ad6d161be85fd86d8ee52e68
kiqueangulo <kiqueangulo@gmail.com>
2022-11-17 13:08:45 -0500

    State problem 242

---
+++ 242-valid-anagram.js
@@ -0,0 +1,16 @@
+/**
+ * Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+ *
+ * An Anagram is a word or phrase formed by rearranging the letters of a different word or
+ * phrase, typically using all the original letters exactly once.
+ *
+ * Constrains:
+ *      s and t consist of lowercase English letters.
+ */
+
+const isAnagram = function (s, t) {}
+
+const s = "anagram"
+const t = "nagaram"
+
+console.log(isAnagram(s, t))

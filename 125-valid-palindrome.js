e38a127ab1ab31d372ea0862678c560e92de3a62
kiqueangulo <kiqueangulo@gmail.com>
2022-11-16 21:50:43 -0500

    State problem 125

---
+++ 125-valid-palindrome.js
@@ -0,0 +1,16 @@
+/**
+ * A phrase is a palindrome if, after converting all uppercase letters into lowercase letters
+ * and removing all non-alphanumeric characters, it reads the same forward and backward.
+ * Alphanumeric characters include letters and numbers.
+ *
+ * Given a string s, return true if it is a palindrome, or false otherwise.
+ *
+ * Constrains:
+ *      s consists only of printable ASCII characters.
+ */
+
+const isPalindrome = function (s) {}
+
+const s = "A man, a plan, a canal: Panama"
+
+console.log(isPalindrome(s)) // true

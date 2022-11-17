663c1224e996a424942bb60768a79db3fe294ae8
kiqueangulo <kiqueangulo@gmail.com>
2022-11-16 22:07:06 -0500

    Problem 125 solved with 1393 ms and 53.5 MB, horrible performance

--- 125-valid-palindrome.js
+++ 125-valid-palindrome.js
@@ -9,7 +9,26 @@
  *      s consists only of printable ASCII characters.
  */
 
-const isPalindrome = function (s) {}
+const isPalindrome = function (s) {
+  const characters = s.toLowerCase().split("")
+  let forward = [],
+    backwards = []
+
+  for (let symbol of characters) {
+    if (symbol.match(/[a-z0-9]/)) {
+      forward.push(symbol)
+      backwards.unshift(symbol)
+    }
+  }
+
+  for (let index in forward) {
+    if (forward[index] !== backwards[index]) {
+      return false
+    }
+  }
+
+  return true
+}
 
 const s = "A man, a plan, a canal: Panama"
 

b47faa429485ba0c8a5433344df08e6b5044b921
kiqueangulo <kiqueangulo@gmail.com>
2022-11-17 02:58:55 -0500

    Problem 217 solved with 82 ms and 51 MB, excellent time complexity

--- 217-contains-duplicate.js
+++ 217-contains-duplicate.js
@@ -3,7 +3,17 @@
  * and return false if every element is distinct.
  */
 
-const containsDuplicate = function (nums) {}
+const containsDuplicate = function (nums) {
+  const set = new Set()
+
+  for (let num of nums) {
+    if (set.has(num)) return true
+
+    set.add(num)
+  }
+
+  return false
+}
 
 const nums = [1, 2, 3, 1]
 

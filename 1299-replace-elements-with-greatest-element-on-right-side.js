455b158102b89754781d90336faab59f8c0fa1f5
kiqueangulo <kiqueangulo@gmail.com>
2022-11-18 12:59:15 -0500

    Problem 1299 solved with 7058 ms and 82.2 MB, low performance

--- 1299-replace-elements-with-greatest-element-on-right-side.js
+++ 1299-replace-elements-with-greatest-element-on-right-side.js
@@ -5,7 +5,16 @@
  * After doing so, return the array.
  */
 
-const replaceElements = function (arr) {}
+const replaceElements = function (arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    arr[i] = Math.max(...arr.slice(i + 1))
+  }
+
+  arr.pop()
+  arr.push(-1)
+
+  return arr
+}
 
 const arr = [17, 18, 5, 4, 6, 1]
 

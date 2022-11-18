09e4e7ceab90c011998fc3ea7039588f7e7a356e
kiqueangulo <kiqueangulo@gmail.com>
2022-11-18 12:50:31 -0500

    State problem 1299

---
+++ 1299-replace-elements-with-greatest-element-on-right-side.js
@@ -0,0 +1,12 @@
+/**
+ * Given an array arr, replace every element in that array with the greatest element among
+ * the elements to its right, and replace the last element with -1.
+ *
+ * After doing so, return the array.
+ */
+
+const replaceElements = function (arr) {}
+
+const arr = [17, 18, 5, 4, 6, 1]
+
+console.log(replaceElements(arr)) // Expected: [18,6,6,6,1,-1]

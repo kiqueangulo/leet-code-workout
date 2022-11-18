36f958118b4fc84893f90ea9976bbb491036e154
kiqueangulo <kiqueangulo@gmail.com>
2022-11-18 13:23:37 -0500

    Problem 1299 second soution with 435 ms and 51.4 MB, better performance linear time

--- 1299-replace-elements-with-greatest-element-on-right-side.js
+++ 1299-replace-elements-with-greatest-element-on-right-side.js
@@ -5,7 +5,8 @@
  * After doing so, return the array.
  */
 
-const replaceElements = function (arr) {
+// Solution 1
+const replaceElements1 = function (arr) {
   for (let i = 0; i < arr.length - 1; i++) {
     arr[i] = Math.max(...arr.slice(i + 1))
   }
@@ -16,6 +17,18 @@ const replaceElements = function (arr) {
   return arr
 }
 
+// Solution 2
+const replaceElements2 = function (arr) {
+  let maxNumenber = -1
+
+  for (let i = arr.length - 1; i >= 0; i--) {
+    ;[arr[i], maxNumenber] = [maxNumenber, Math.max(maxNumenber, arr[i])]
+  }
+
+  return arr
+}
+
 const arr = [17, 18, 5, 4, 6, 1]
 
-console.log(replaceElements(arr)) // Expected: [18,6,6,6,1,-1]
+console.log(replaceElements1(arr)) // Expected: [18,6,6,6,1,-1]
+console.log(replaceElements2(arr))

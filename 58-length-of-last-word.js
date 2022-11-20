e370638bd99a8998d0d53949c3ace6d37bf2c9a7
kiqueangulo <kiqueangulo@gmail.com>
2022-11-20 13:47:37 -0500

    State problem 58

---
+++ 58-length-of-last-word.js
@@ -0,0 +1,16 @@
+/**
+ * Given a string s consisting of words and spaces, return the length of the last word
+ * in the string.
+ *
+ * A word is a maximal substring consisting of non-space characters only.
+ *
+ * Constrains:
+ *      s consists of only English letters and spaces ' '.
+ *      There will be at least one word in s.
+ */
+
+const lengthOfLastWord = function (s) {}
+
+const s = "   fly me   to   the moon  "
+
+console.log(lengthOfLastWord(s))

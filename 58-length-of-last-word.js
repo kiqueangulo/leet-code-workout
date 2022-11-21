e29c2cf304609df10ea66c75a252cc86e4d7673e
kiqueangulo <kiqueangulo@gmail.com>
2022-11-21 03:44:49 -0500

    Problem 58 second solution with 63 ms and 41.9 MB, amazing performance

--- 58-length-of-last-word.js
+++ 58-length-of-last-word.js
@@ -9,25 +9,24 @@
  *      There will be at least one word in s.
  */
 
-const lengthOfLastWord = function (s) {
+// Solution 1: Time -> O(n) (The whole string is used in each operation, except in pop)
+const lengthOfLastWord1 = function (s) {
   return s
     .split(" ")
-    .filter(element => element)
+    .filter(item => item != "")
     .pop().length
+}
+
+//Solution2: Time -> O(n) (The whole string is manipulated only once)
+const lengthOfLastWord2 = function (s) {
+  const splited = s.split(" ")
+
+  for (let i = splited.length - 1; i >= 0; i--) {
+    if (splited[i]) return splited[i].length
+  }
 }
 
 const s = "   fly me   to   the moon  "
 
-console.log(lengthOfLastWord(s))
-
-/*
-  let noSpace = ""
-
-  for (let i = 0; i < s.length - 1; i++) {
-    if (s[i] === " " && s[i + 1] === " ") continue
-
-    noSpace += s[i]
-  }
-
-  return noSpace
-*/
+console.log(lengthOfLastWord1(s))
+console.log(lengthOfLastWord2(s))

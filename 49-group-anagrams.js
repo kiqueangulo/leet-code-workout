99943557eae178f915be3365a2a26dd0785d7b41
kiqueangulo <kiqueangulo@gmail.com>
2022-11-24 02:17:35 -0500

    State problem 49

---
+++ 49-group-anagrams.js
@@ -0,0 +1,15 @@
+/**
+ * Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+ *
+ * An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase,
+ * typically using all the original letters exactly once.
+ *
+ * Constrains:
+ *      strs[i] consists of lowercase English letters.
+ */
+
+const groupAnagrams = function (strs) {}
+
+const strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
+
+console.log(groupAnagrams(strs))

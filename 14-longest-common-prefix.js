7cd9b8968934b2a6263f001aefd942aebc1aa133
kiqueangulo <kiqueangulo@gmail.com>
2022-11-24 02:01:45 -0500

    Problem 14 solved with 75 ms and 43.3 MB; good performance in time complexity, below average in space complexity

--- 14-longest-common-prefix.js
+++ 14-longest-common-prefix.js
@@ -7,7 +7,20 @@
  *      strs[i] consists of only lowercase English letters.
  */
 
-const longestCommonPrefix = function (strs) {}
+// Time -> O(n.m); Space -> O(n)
+const longestCommonPrefix = function (strs) {
+  let prefix = ""
+
+  for (let i = 0; i < strs[0].length; i++) {
+    for (let j = 1; j < strs.length; j++) {
+      if (strs[0][i] !== strs[j][i]) return prefix
+    }
+
+    prefix += strs[0][i]
+  }
+
+  return prefix
+}
 
 const strs = ["flower", "flow", "flight"]
 

b0eed37b0dbe5a99455039c86bf3dc66a853f42b
kiqueangulo <kiqueangulo@gmail.com>
2022-11-24 01:36:33 -0500

    State problem 14

---
+++ 14-longest-common-prefix.js
@@ -0,0 +1,14 @@
+/**
+ * Write a function to find the longest common prefix string amongst an array of strings.
+ *
+ * If there is no common prefix, return an empty string "".
+ *
+ * Constrains:
+ *      strs[i] consists of only lowercase English letters.
+ */
+
+const longestCommonPrefix = function (strs) {}
+
+const strs = ["flower", "flow", "flight"]
+
+console.log(longestCommonPrefix(strs))

71bbfb57a79eebad0bcb597c8883e5894344d267
kiqueangulo <kiqueangulo@gmail.com>
2022-11-24 03:35:15 -0500

    Problem 128 solved with 193 ms and 51.3 MB
    Average performance overall; excellent space complexity

--- 128-longest-consecutive-sequence.js
+++ 128-longest-consecutive-sequence.js
@@ -5,8 +5,30 @@
  * You must write an algorithm that runs in O(n) time.
  */
 
-const longestConsecutive = function (nums) {}
+// Time -> O(n) (overall); Space -> O(n)
+const longestConsecutive = function (nums) {
+  if (!nums.length) return 0
 
-const nums = [100, 4, 200, 1, 3, 2]
+  // Time -> O(n.logn) (probably because of the size of test cases,
+  // this time complexity can be ommited, not sure)
+  const sorted = nums.sort((a, b) => a - b)
+  const length = []
+  let count = 1
+
+  for (let i = 0; i < sorted.length - 1; i++) {
+    if (sorted[i] === sorted[i + 1]) continue
+
+    if (sorted[i] + 1 !== sorted[i + 1]) {
+      length.push(count)
+      count = 1
+    } else count++
+  }
+
+  length.push(count)
+
+  return Math.max(...length)
+}
+
+const nums = [1, 2, 0, 1]
 
 console.log(longestConsecutive(nums))

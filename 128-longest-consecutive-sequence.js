d407d70b9f62ab9e4a055638e46dcc1e5ec98d91
kiqueangulo <kiqueangulo@gmail.com>
2022-11-24 02:57:55 -0500

    State problem 128

---
+++ 128-longest-consecutive-sequence.js
@@ -0,0 +1,12 @@
+/**
+ * Given an unsorted array of integers nums, return the length of the longest consecutive
+ * elements sequence.
+ *
+ * You must write an algorithm that runs in O(n) time.
+ */
+
+const longestConsecutive = function (nums) {}
+
+const nums = [100, 4, 200, 1, 3, 2]
+
+console.log(longestConsecutive(nums))
